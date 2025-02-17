09cde9b4b725fa13a600c4bc1c04da6f56b55fac
Enes Talha <enes.talha@hotmail.com.tr>
2025-02-17 22:54:01 +0300

    Add project files.

---
+++ DropletHelper/PlaylistHolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropletHelper
+{
+    internal class PlaylistHolder
+    {
+        string playlistName;
+
+        Dictionary<string, Song> ListOfMusic = new Dictionary<string, Song>();
+
+
+
+    }
+}
---
+++ DropletHelper/Song.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropletHelper
+{
+    internal class Song
+    {
+
+        string SongName { get; }
+
+        string SongLink { get;}
+
+        string SongCreator { get; }
+        Dictionary<string,int> Token {get;}
+
+        public Song(string songname,string link,string creator)
+        {
+            
+            SongName = songname;
+            SongLink = link;
+            SongCreator = creator;
+
+            TokenizeSong(SongName,SongCreator);
+
+
+
+        }
+        public Song(string songname,string creator)
+        {
+
+
+
+        }
+        public Song(string songname)
+        {
+
+
+
+        }
+
+        private void TokenizeSong(string name,string creator)
+        {
+
+
+        }
+
+        private void TokenizeSong(string name)
+        {
+
+
+        }
+
+    }
+}
