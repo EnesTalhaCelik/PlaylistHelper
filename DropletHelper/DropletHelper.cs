1d3ff356fd2958c2dad3710b7647a67eef05357e
Enes Talha <enes.talha@hotmail.com.tr>
2025-02-18 02:16:04 +0300

    token txt deleted found better way

---
+++ DropletHelper/DropletHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+
+namespace DropletHelper
+{
+    internal static class DropletHelper
+    {
+        internal static string SpotifyToken;
+
+        private static async void initializeProgram()
+        {
+
+
+
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                SpotifyToken = data.SpotiToken;
+
+            }
+
+        }
+
+        internal sealed class JSONStructure
+        {
+            internal string SpotiToken { get; set; }
+            
+        }
+
+
+
+
+
+
+    }
+}
